27bf7cea04007568109fda3240e081fbe7e3fd20
JiSeong Kim <12180543@inha.edu>
2024-05-06 01:53:00 +0900

    feat: useFetch 훅 생성

---
+++ front/src/hooks/useFetch.jsx
@@ -0,0 +1,36 @@
+import axios from "axios";
+import { useState } from "react";
+
+export const useFetch = () => {
+    const Server_IP = process.env.REACT_APP_Server_IP;
+
+    const [result, setResult] = useState('');
+
+    const getReq = async ({url, method, data, token}) => {
+        const headers = {};
+        if (token) {
+            headers['Authorization'] = `Bearer ${token}`;
+        }
+        try {
+            const response = await axios.get(`${Server_IP}/${url}`, { headers });
+            setResult(response.data);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    const postReq = async ({url, method, data, token}) => {
+        const headers = {};
+        if (token) {
+            headers['Authorization'] = `Bearer ${token}`;
+        }
+        try {
+            const response = await axios.post(`${Server_IP}/${url}`, data, { headers }); // 여기서 axios.get을 axios.post로 변경해야 할 것 같습니다.
+            setResult(response.data);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    return { result, getReq, postReq };
+}
