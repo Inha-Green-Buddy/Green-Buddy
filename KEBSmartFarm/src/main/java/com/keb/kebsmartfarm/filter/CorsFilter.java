963391c782518f0700012c94409e05616f3129b8
JGM <cnxw4570123@gmail.com>
2024-05-31 16:08:56 +0900

    fix : add allowedOrigin presets to CorsFilter

--- KEBSmartFarm/src/main/java/com/keb/kebsmartfarm/filter/CorsFilter.java
+++ KEBSmartFarm/src/main/java/com/keb/kebsmartfarm/filter/CorsFilter.java
@@ -3,15 +3,21 @@ package com.keb.kebsmartfarm.filter;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.Set;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+@Slf4j
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilter implements Filter {
+    private static final Set<String> ALLOWED_ORIGINS = Set.of("http://localhost:3000",
+            "https://sf002.netlify.app:3000");
+
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
         Filter.super.init(filterConfig);
@@ -23,11 +29,17 @@ public class CorsFilter implements Filter {
     }
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
         HttpServletRequest req = (HttpServletRequest) request;
         HttpServletResponse res = (HttpServletResponse) response;
+        String origin = req.getHeader("Origin");
+        log.info(origin);
+        if (!ALLOWED_ORIGINS.contains(origin)) {
+            return;
+        }
 
-        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Allow-Origin", origin);
         res.setHeader("Access-Control-Allow-Credentials", "true");
         res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
         res.setHeader("Access-Control-Max-Age", "3600");
@@ -36,7 +48,7 @@ public class CorsFilter implements Filter {
 
         if ("OPTIONS".equalsIgnoreCase(req.getMethod())) {
             res.setStatus(HttpServletResponse.SC_OK);
-        }else {
+        } else {
             chain.doFilter(req, res);
 
         }
